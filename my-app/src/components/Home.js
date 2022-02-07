de4bad62d3ec95e42c1be00af5466be0a333772a
Mdnd143 <mohd7210357259@gmail.com>
2022-02-07 12:32:56 +0530

    logics part done

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [inputWord, setInputWord] = useState("")
+  const [search, setSearch] = useState("")
+  const [inputMeaning, setInputMeaning] = useState("") 
+  const [insertWord, setInsertWord] = useState(getDataLocal());
+  function getDataLocal(){
+     let localData = localStorage.getItem("dictionary")
+     if(localData){
+     return JSON.parse(localStorage.getItem("dictionary"))
+     }
+     else{
+       return [];
+     }
+  }
+  useEffect(() => {
+           localStorage.setItem("dictionary", JSON.stringify(insertWord))
+  }, [insertWord]);
+  
+
+  function addWord(e){
+             e.preventDefault();
+             let insertWords = {
+               word: inputWord,
+               meaning: inputMeaning
+             }
+             
+             if(!inputWord){
+ 
+             }
+             else{
+               setInsertWord([...insertWord, insertWords])
+             setInputWord("")
+             setInputMeaning("")
+             }
+  }
+
   return <div className='container'>
       <h1 className='my-5 mx-2'>Type your word below</h1>
       <div className="input-group input-group-lg">
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
+  <input type="text" className="form-control" onChange={(e)=>{setSearch(e.target.value)}} aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
 </div>
-      <div> <h2 className='my-3'>Word : Meaning kjbvkjbvkjbkvbjkjbkjf kbekjkbjebiebkjb jebovb</h2></div>
-      <div> <h2 className='my-3'>Word : Meaning kjbvkjbvkjbkvbjkjbkjf kbekjkbjebiebkjb jebovb</h2></div>
-      <div> <h2 className='my-3'>Word : Meaning kjbvkjbvkjbkvbjkjbkjf kbekjkbjebiebkjb jebovb</h2></div>
-      <div> <h2 className='my-3'>Word : Meaning kjbvkjbvkjbkvbjkjbkjf kbekjkbjebiebkjb jebovb</h2></div>
-
+      <div> <div id='notes'>{
+                  insertWord.filter((val)=>{
+                    if(search===""){
+                      return val
+                    }
+                    else if(val.word.toLowerCase().includes(search.toLowerCase())){
+                      return val
+                    }
+                  }).map((elem, index)=>{
+                    return(
+                      <h4 className='my-3' key={index}>{elem.word} : {elem.meaning}</h4>
+                    )
+                  })  
+} </div></div>
   </div>;
 }
--- my-app/src/components/Newword.js
+++ my-app/src/components/Newword.js
@@ -1,11 +1,81 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 export default function Newword() {
+ const [inputWord, setInputWord] = useState("")
+ const [inputMeaning, setInputMeaning] = useState("") 
+ const [insertWord, setInsertWord] = useState(getDataLocal());
+ const [sortType, setSortType] = useState("asc");
+ function getDataLocal(){
+    let localData = localStorage.getItem("dictionary")
+    if(localData){
+    return JSON.parse(localStorage.getItem("dictionary"))
+    }
+    else{
+      return [];
+    }
+ }
+ useEffect(() => {
+          localStorage.setItem("dictionary", JSON.stringify(insertWord))
+ }, [insertWord]);
+ 
+ function addWord(e){
+            e.preventDefault();
+            let currentTime = String(new Date());
+            let myTime = currentTime.substring(4,21)
+            let insertWords = {
+              word: inputWord,
+              meaning: inputMeaning,
+              time: myTime
+            }
+            if(!inputWord || !inputMeaning){
+                  alert("both fields required")
+            }
+            else{
+              insertWord.filter((val)=>{
+                  if(val.word.toLowerCase().includes(inputWord.toLowerCase())){
+                   alert("already in our data base")
+                  }
+                  else{
+                    setInsertWord([...insertWord, insertWords])
+                setInputWord("")
+                setInputMeaning("")
+                  }
+                })  
+          }
+            
+
+            // insertWord.filter((val)=>{
+            //   if(!inputWord){}
+            //   else if(val.word.toLowerCase().includes(inputWord.toLowerCase())){
+            //     alert("this word alraedy taken")
+            //   }
+            //   else if(inputMeaning!==""){
+            //     setInsertWord([...insertWord, insertWords])
+            // setInputWord("")
+            // setInputMeaning("")
+            //   }
+            // })           
+ }
+  const sortedData = insertWord.sort((a,b)=>{
+    const isReversed = (sortType==="asc")?1:-1;
+    return isReversed*a.word.localeCompare(b.word)
+  })
+
+
   return <div className='container'>
       <h1 className='mx-2 my-5'> Add your word to our database</h1>
       <div className="input-group mb-3">
-  <input type="text" className="form-control mx-2" placeholder="Word" />
-  <input type="text" className="form-control mx-2" placeholder="Meaning"/>
+  <div className='ml-2 w-20'><input type="text" value={inputWord} onChange={(e)=>{setInputWord(e.target.value)}} className="form-control" id='inputword' placeholder="Word" /></div>
+  <div className='w-50 mx-2'><input type="text" 
+  value={inputMeaning}  onChange={(e)=>{setInputMeaning(e.target.value)}} 
+  className="form-control" id='inputmeaning' placeholder="Meaning"/></div>
+  <button type="button" id='addbtn' onClick={addWord} className="btn btn-info text-light">Add to Dictionary</button>
 </div>
+<div id='notes'>{
+                  sortedData.map((elem, index)=>{
+                    return(
+                      <h4 className='my-3' key={index}>{elem.word} : {elem.meaning}<small>{elem.time}</small></h4>                      
+                    )
+                  })  
+} </div>
   </div>;
 }
